082d5566c67aa3145fd02af2e370ab5fde301181
MrAcarus <dimasoftwer@gmail.com>
2021-06-25 22:16:36 +0300

    .

---
+++ src/Card.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Card {
+
+    private String phrase;
+    private String meaning;
+    private String synonyms;
+
+    private final String PHRASE_TEXT = "Phrase: ";
+    private final String MEANING_TEXT = "Meaning: ";
+    private final String SYNONYMS_TEXT = "Synonyms: ";
+
+
+    Card(String phrase, String meaning, String synonyms) {
+        this.phrase = phrase;
+        this.meaning = meaning;
+        this.synonyms = synonyms;
+    }
+
+    public void printCard() {
+        System.out.println(PHRASE_TEXT + phrase);
+        System.out.println(MEANING_TEXT + meaning);
+        System.out.println(SYNONYMS_TEXT + synonyms);
+    }
+
+    public void changeCardData(String phrase, String meaning, String synonyms) {
+        this.phrase = phrase;
+        this.meaning = meaning;
+        this.synonyms = synonyms;
+    }
+
+    public String getPhrase() {
+        return phrase;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static ArrayList<Card> Cards = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        while (Interfaces.menu()) {
+            System.out.println();
+        }
+    }
+
+}
